2cc200b332937350d778035f52b7cb391dcaaca5
Bekarys <50898152+maagauiya@users.noreply.github.com>
2021-10-10 17:36:55 +0600

    Update main.py

--- test/main.py
+++ test/main.py
@@ -1,5 +1,5 @@
 import parserwithsel
 scrapper=parserwithsel
-name_of_coin=str(input())
 print('Enter the name of the cryptocurrency you want to see the latest news:')
+name_of_coin=str(input())
 scrapper.get_datar(name_of_coin)
